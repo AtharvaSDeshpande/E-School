b015986323573556758552a54406e2d4b5414f07
Atharva <atharvadeshpande174@gmail.com>
2021-08-08 00:52:19 +0530

    Completed Forgot password functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,129 +12,166 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 function Login() {
-  
+    const [forgot, setForgot] = useState(false);
     const [passwordHidden, setPasswordHidden] = useState(true);
     const [passwordType, setPasswordType] = useState("password");
-    const changePasswordTypeVisible = () => {    
-            setPasswordType("text");
-            setPasswordHidden(false);
+    const changePasswordTypeVisible = () => {
+        setPasswordType("text");
+        setPasswordHidden(false);
     }
 
     const changePasswordTypeHidden = () => {
-            setPasswordType("password");
-            setPasswordHidden(true);
+        setPasswordType("password");
+        setPasswordHidden(true);
     }
- 
-    const [{user},dispatch] = useStateValue();
+
+    const [{ user }, dispatch] = useStateValue();
 
     const formik = useFormik({
 
-		initialValues: {
-			email: '',
-			password: '',
-		},
-		
-		validationSchema: Yup.object({
+        initialValues: {
+            email: '',
+            password: '',
+        },
 
-		
+        validationSchema: Yup.object({
 
-			email: Yup.string().email('Invalid email address').required('Required'),
-			password: Yup.string()
-				.min(8, 'Must be 8 characters or more')
 
-				.required('Required'),
-			
-			
-		}),
 
-		onSubmit: values => {
-			auth.signInWithEmailAndPassword(values.email,values.password).then((userCredential) => {
+            email: Yup.string().email('Invalid email address').required('Required'),
+            password: Yup.string()
+                .min(8, 'Must be 8 characters or more')
+
+                .required('Required'),
+
+
+        }),
+
+        onSubmit: values => {
+            auth.signInWithEmailAndPassword(values.email, values.password).then((userCredential) => {
                 // Signed in
-                
-               if(userCredential.user.emailVerified)
-               {
+
+                if (userCredential.user.emailVerified) {
                     console.log(userCredential.user);
                     dispatch({
                         type: actionTypes.SET_USER,
                         user: userCredential.user,
                     });
-               }
-              
-                else
-                {
+                }
+
+                else {
                     dispatch({
                         type: actionTypes.SET_USER,
                         user: null,
                     });
                     auth.signOut()
-                    
+
                     alert("Email not Verified")
-                    
 
-                }   
+
+                }
                 // ...
-              })
-              .catch(alert);
-			
-		},
+            })
+                .catch(alert);
 
-	});
+        },
 
-    
+    });
+   const [forgotEmail,setForgotEmail] = useState(null);
+    const changePassword = (e) => {
+        
+            e.preventDefault();
+            auth.sendPasswordResetEmail(forgotEmail).then(()=>{
+                      alert("Change password link successfully sent to the email ID") 
+                   }).catch(alert);
+            setForgotEmail(null)
+      
+        
+    }
+
     return (
         <div className="login">
+            {!forgot ? (
+                <form className="loginForm" onSubmit={formik.handleSubmit}>
 
-            <form className="loginForm" onSubmit={formik.handleSubmit}>
+                    <img className="login_img" src={logo} alt="Logo" />
+                    <div className="login__header"><h1>Login</h1>or <Link to="/createaccount" className="link">Create Account</Link></div>
 
-                <img className="login_img" src={logo} alt="Logo" />
-                <div className="login__header"><h1>Login</h1>or <Link to="/createaccount" className="link">Create Account</Link></div>
-                
-               {/* Email */}
-				<div className="inputBlock">
-					<p className="inputBlock__label"><b>Email</b></p>
-					<input id="email" className="inputBlock__input" type="email" required
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-						value={formik.values.email}
-					/>
-					{formik.touched.email && formik.errors.email ? (<p className="extraLabel" >{formik.errors.email}</p>) : (<p className="hidden"></p>)}
-				</div>
-				{/* Password */}
+                    {/* Email */}
+                    <div className="inputBlock">
+                        <p className="inputBlock__label"><b>Email</b></p>
+                        <input id="email" className="inputBlock__input" type="email" value ={forgotEmail}  required
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                        />
+                        {formik.touched.email && formik.errors.email ? (<p className="extraLabel" >{formik.errors.email}</p>) : (<p className="hidden"></p>)}
+                    </div>
+                    {/* Password */}
 
-				<div className="inputBlock">
-					<p className="inputBlock__label"><b>Password</b></p>
-					<div>
-						<div className="passwordInputBox">
-							<input id="password" className="inputBlock__input inputBlock__password" type={passwordType} required
-								
-								onChange={formik.handleChange}
-								onBlur={formik.handleBlur}
-								value={formik.values.password}
-							/>
-							<div className="inputBlock__input__button" onMouseDown={changePasswordTypeVisible} onMouseUp={changePasswordTypeHidden}>
-								{passwordHidden ? (
-									<VisibilityOffIcon className="icon" />
-								) : (
-									<VisibilityIcon className="icon" />
-								)}
-							</div>
-							
-						</div>
-						{formik.touched.password && formik.errors.password ? (<p className="extraLabel" >{formik.errors.password}</p>) : (<p className="hidden"></p>)}
-					</div>
-				</div>
-                <div className = "buttons">
-                    <button type="submit" variant="contained" color = "secondary" className="login_submit">
+                    <div className="inputBlock">
+                        <p className="inputBlock__label"><b>Password</b></p>
+                        <div>
+                            <div className="passwordInputBox">
+                                <input id="password" className="inputBlock__input inputBlock__password" type={passwordType} required
+
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.password}
+                                />
+                                <div className="inputBlock__input__button" onMouseDown={changePasswordTypeVisible} onMouseUp={changePasswordTypeHidden}>
+                                    {passwordHidden ? (
+                                        <VisibilityOffIcon className="icon" />
+                                    ) : (
+                                        <VisibilityIcon className="icon" />
+                                    )}
+                                </div>
+
+                            </div>
+                            {formik.touched.password && formik.errors.password ? (<p className="extraLabel" >{formik.errors.password}</p>) : (<p className="hidden"></p>)}
+                        </div>
+                    </div>
+                    <div className="buttons">
+                        <button type="submit" variant="contained" color="secondary" className="login_submit">
                             Login
-                    </button>
-                    <Link className = "link" to = "/forgot">Forgot password?</Link>
+                        </button>
+                        <p className="link" onClick = {()=>{setForgot(true)}}>Forgot password?</p>
 
-                </div>
-
-                
+                    </div>
 
 
-            </form>
+
+
+                </form>
+            ) : (<div className="login">
+                <form className="loginForm" >
+
+                    <img className="login_img" src={logo} alt="Logo" />
+                    <div className="login__header"><h1>Login</h1>or <Link to="/createaccount" className="link">Create Account</Link></div>
+
+                    {/* Email */}
+                    <div className="inputBlock">
+                        <p className="inputBlock__label"><b>Email</b></p>
+                        <input id="email" className="inputBlock__input" type="email" required
+                            onChange={(e) => {setForgotEmail(e.target.value)}}
+                            
+                        />
+                        {/* {forgotFormik.touched.email && forgotFormik.errors.email ? (<p className="extraLabel" >{forgotFormik.errors.email}</p>) : (<p className="hidden"></p>)} */}
+                    </div>
+                   
+                    <div className="buttons">
+                        <button type="submit" variant="contained" color="secondary" className="login_submit" onClick = {changePassword}>
+                            CHANGE PASSWORD
+                        </button>
+                        <p className="link" onClick = {()=>{setForgot(false)}}>Login?</p>
+
+                    </div>
+
+
+
+
+                </form>
+            </div>)}
 
         </div>
     )

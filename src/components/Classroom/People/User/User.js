29b62cac6cb6b82c3d43a21de93fab0287324770
Atharva <atharvadeshpande174@gmail.com>
2021-08-13 00:00:40 +0530

    Updated User view of People page

--- src/components/Classroom/People/User/User.js
+++ src/components/Classroom/People/User/User.js
@@ -1,14 +1,22 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, Divider } from '@material-ui/core'
+import { MoreVert } from '@material-ui/icons'
 import React from 'react'
 import './User.css'
-function User({isTeacher,displayName,photoURL,isTeacherArea}) {
+function User({ isTeacher, displayName, photoURL, isTeacherArea }) {
     return (
-        <div className = "user">
-            {isTeacher && !isTeacherArea?(
-                <input type = "checkbox" className = "user__input"/>
-            ):null}
-            <div className = "user__left"><Avatar  className={`user__avatar ${photoURL}`}>{displayName.split(' ')[0][0] + displayName.split(' ')[1][0]}</Avatar></div>            
-            <p className = "user__name">{displayName}</p>
+        <div>
+            {isTeacher && !isTeacherArea ? (
+                <div className="user pointer">
+                    <input type="checkbox" className="user__input" />
+                    <div className="user__left"><Avatar className={`user__avatar ${photoURL}`}>{displayName.split(' ')[0][0] + displayName.split(' ')[1][0]}</Avatar></div>
+                    <p className="user__name">{displayName}</p>
+                    <MoreVert className = "user__moreVert"/>
+                </div>
+            ) : (<div className="user">
+                <div className="user__left"><Avatar className={`user__avatar ${photoURL}`}>{displayName.split(' ')[0][0] + displayName.split(' ')[1][0]}</Avatar></div>
+                <p className="user__name">{displayName}</p>
+            </div>)}
+            
         </div>
     )
 }
